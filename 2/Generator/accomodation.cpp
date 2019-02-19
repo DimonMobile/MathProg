d53a241617b51b0767a8d8b26b29c8f610e58cf7
Dimon Mobile <mobik.dimka@gmail.com>
2019-02-19 16:29:30 +0300

    accomodation base methods

--- 2/Generator/accomodation.cpp
+++ 2/Generator/accomodation.cpp
@@ -1,18 +1,35 @@
 #include "accomodation.h"
 
+#include "combination.h"
+#include "permutation.h"
+
 namespace Generator
 {
 
 Accomodation::Accomodation(uint n, uint m)
 {
     m_combinationGenerator = new Generator::Combination(n, m);
-    m_permutationGenerator = new Generator::Permutation(static_cast<int>(n));
+    m_permutationGenerator = new Generator::Permutation(static_cast<int>(m));
 }
 
 Accomodation::~Accomodation()
 {
     delete m_combinationGenerator;
     delete m_permutationGenerator;
+}
+
+void Accomodation::reset()
+{
+    m_currentPermutation = m_permutationGenerator->count();
+}
+
+std::vector<int> Accomodation::next()
+{
+//    if (m_currentPermutation == m_permutationGenerator->count())
+//    {
+//        m_currentPermutation = 0;
+//        m_combinationGenerator->next();
+//    }
 }
 
 
--- 2/Generator/accomodation.h
+++ 2/Generator/accomodation.h
@@ -1,11 +1,15 @@
 #ifndef ACCOMODATION_H
 #define ACCOMODATION_H
 
-#include "combination.h"
-#include "permutation.h"
+#include <vector>
+
+typedef unsigned int uint;
 
 namespace Generator
 {
+
+class Permutation;
+class Combination;
 
 class Accomodation
 {
@@ -13,9 +17,13 @@ public:
     Accomodation(uint n, uint m);
     ~Accomodation();
 
+    void reset();
+    std::vector<int> next();
+
 private:
     Generator::Combination *m_combinationGenerator;
     Generator::Permutation *m_permutationGenerator;
+    uint m_currentPermutation;
 };
 
 } // namespace Generator
