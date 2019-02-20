c13e25a553f86769321a801a774ac1aa5b913724
Dimon Mobile <mobik.dimka@gmail.com>
2019-02-20 13:08:01 +0300

    Variant 9. Lab complete

--- 2/main.cpp
+++ 2/main.cpp
@@ -1,17 +1,41 @@
 #include <iostream>
+#include <iomanip>
+#include <ctime>
 
-#include "Generator/accomodation.h"
+#include "Generator/Task/salesman.h"
+
+#define N 13
 
 int main()
 {
-    Generator::Accomodation generator(4, 2);
-    for(size_t i = 0 ; i < generator.count(); ++i)
+    uint d[N][N];
+    std::cout << "*\tSource array: " << std::endl;
+    for(int i = 0 ; i < N; ++i)
     {
-        std::vector<uint> current = generator.next();
-        for(auto item : current)
-            std::cout << item << ' ';
+        for(int j = 0 ; j < N; ++j)
+        {
+            if (rand() % 12*12 <= 2)
+            {
+                d[i][j] = Generator::Constants::infinity;
+                std::cout << std::setw(5) << "INF";
+            }
+            else
+            {
+                d[i][j] = rand() % 290 + 10;
+                std::cout << std::setw(5) << d[i][j];
+            }
+        }
         std::cout << std::endl;
     }
+    std::cout << "*\tTask resolving: " << std::endl;
 
+    for(int i = 5; i <= 12; ++i)
+    {
+        uint r[N];
+        clock_t startTime = clock();
+
+        std::cout << std::setw(3) << i << " " << Generator::Task::salesman(i, reinterpret_cast<const uint*>(d), r) << " ";
+        std::cout << std::setw(8) << (clock() - startTime) / static_cast<double>(CLOCKS_PER_SEC) << std::endl;
+    }
     return 0;
 }
