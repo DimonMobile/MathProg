d2c24cce67f35e5f7db6bf81f9d970fcb703cb1b
Dimon Mobile <mobik.dimka@gmail.com>
2019-04-15 18:09:02 +0300

    License updated

--- 3/main.cpp
+++ 3/main.cpp
@@ -1,10 +1,20 @@
-/*
- * File created 14.04.2019
- * implementation of algorithm described here: http://galyautdinov.ru/post/zadacha-kommivoyazhera
+/*****************************************************************************
+ * main.cpp
  *
- * This project created by Plotnikov Dmitry | mobik.dimka@gmail.com
- * and placed at https://github.com/DimonMobile/MathProg
-*/
+ * Created: 13.04.2019 2019 by d.plotnikov
+ *
+ * Copyright © 2019 Dmitry Plotnikov. All rights reserved.
+ *
+ * This file may be distributed under the terms of GNU Public License version
+ * 3 (GPL v3) as defined by the Free Software Foundation (FSF). A copy of the
+ * license should have been included with this file, or the project in which
+ * this file belongs to. You may also find the details of GPL v3 at:
+ * http://www.gnu.org/licenses/gpl-3.0.txt
+ *
+ * If you have any questions regarding the use of this file, feel free to
+ * contact the author of this file, or the owner of the project in which
+ * this file belongs to.
+*****************************************************************************/
 
 #include <iostream>
 #include <iomanip>
